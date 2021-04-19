1a598fac9ea5eda83d28fc8bee782d9defd7eda6
evelyn Alves <evelynsba@gmail.com>
2021-04-19 20:57:47 -0300

    criar background

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("snake");
+let context  = canvas.getContext("2d");
+let box = 32;
+
+//creating the background
+
+function criarBG(){
+
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+criarBG();
